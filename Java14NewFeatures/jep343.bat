207d5497bc5d9d4d4a1eb635afc3666c4c30e512
Serkan Taş <serkan.tas@likyateknoloji.com>
2020-04-11 20:35:06 +0300

    initial commit

---
+++ Java14NewFeatures/jep343.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Java\AdoptOpenJDK\jdk-14.0.0.36-openj9\bin\jpackage" --name FeaturesJEP305 --input build/jar --main-jar FeaturesJEP305.jar
+
---
+++ Java14NewFeatures/src/com/likya/preview/FeaturesJEP305.java
@@ -0,0 +1,30 @@
+package com.likya.preview;
+
+public class FeaturesJEP305 {
+
+	/**
+	 *  JDK Enhancement Proposal JEP-305
+	 *  Pattern Matching for instanceof (Preview)
+	 * @param args
+	 */
+		
+	
+	public static void main(String[] args) {
+
+		Object obj = "Thisi the test of JEP-305";
+		
+		// Before 
+		if (obj instanceof String) {
+		    String s = (String) obj;
+		    System.out.println("Length of string : "  + s.length());
+		}
+		
+		// After
+		if (obj instanceof String s) {
+		    System.out.println("Length of string : "  + s.length());
+		}
+		    
+
+	}
+
+}
