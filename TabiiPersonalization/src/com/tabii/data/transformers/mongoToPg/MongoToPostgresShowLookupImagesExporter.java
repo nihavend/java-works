0c99d1abf44e7e9b8782d54d00c12ee4c16be417
Serkan Taş <serkan.tas@trt.net.tr>
2025-10-10 20:07:53 +0300

    .

--- TabiiPersonalization/src/com/tabii/data/transformers/mongoToPg/MongoToPostgresShowLookupImagesExporter.java
+++ TabiiPersonalization/src/com/tabii/data/transformers/mongoToPg/MongoToPostgresShowLookupImagesExporter.java
@@ -19,7 +19,7 @@ import com.tabii.utils.CommonUtils;
 import com.tabii.utils.MongoProperties;
 import com.tabii.utils.PgProperties; 
 
-public class MongoToPostgresShowImagesExporter {
+public class MongoToPostgresShowLookupImagesExporter {
 
    
     public  static final Map<String, String> imageMap = Map.ofEntries(
@@ -40,6 +40,11 @@ public class MongoToPostgresShowImagesExporter {
     
     public static void migrate() {
     	
+    	getShowImages();
+    	getLookupImages();
+    }
+    
+    private static void getShowImages() {
     	PgProperties pgProperties = CommonUtils.getPgConnectionProps();
     	MongoProperties mongoProperties = CommonUtils.getMongoConnectionProps();
     	String mongoCollection = "shows"; // Mongo collection
@@ -60,11 +65,17 @@ public class MongoToPostgresShowImagesExporter {
                     Document fields = doc.get("fields", Document.class);
                     if (fields == null) continue;
 
+                    if(fields.toJson().toString().contains("26087_0-0-144-144.jpeg")) {
+                    	System.out.println("Bingo....");
+                    }
+                    
                     for (String key : imageMap.keySet()) {
                         Document imageDoc = fields.get(key, Document.class);
                         if (imageDoc != null) {
                             // String type = imageDoc.getString("type");
                             String fileName = imageDoc.getString("fileName");
+
+                            
                             String title = imageDoc.getString("title");
 
                             if (fileName != null) { // filename is unique, skip nulls
@@ -85,4 +96,51 @@ public class MongoToPostgresShowImagesExporter {
         }
     }
     
+    private static void getLookupImages() {
+    	PgProperties pgProperties = CommonUtils.getPgConnectionProps();
+    	MongoProperties mongoProperties = CommonUtils.getMongoConnectionProps();
+    	String mongoCollection = "lookup"; // Mongo collection
+    	
+        try (MongoClient mongoClient = MongoClients.create(mongoProperties.getMongoUri());
+				Connection pgConn = DriverManager.getConnection(pgProperties.getDbUrl(), pgProperties.getDbUser(),
+						pgProperties.getDbPassword())) {
+
+        	MongoDatabase mongoDb = mongoClient.getDatabase(mongoProperties.getMongoDb());
+			MongoCollection<Document> collection = mongoDb.getCollection(mongoCollection);
+			
+            // Query all documents
+            FindIterable<Document> docs = collection.find();
+
+            String insertSQL = "INSERT INTO images (image_type, filename, title) VALUES (?, ?, ?) ON CONFLICT (filename) DO NOTHING";
+            try (PreparedStatement ps = pgConn.prepareStatement(insertSQL)) {
+                for (Document doc : docs) {
+                    Document fields = doc.get("fields", Document.class);
+                    if (fields == null) continue;
+                    
+                    for (String key : imageMap.keySet()) {
+                        Document imageDoc = fields.get(key, Document.class);
+                        if (imageDoc != null) {
+                            // String type = imageDoc.getString("type");
+                            String fileName = imageDoc.getString("fileName");
+
+                            
+                            String title = imageDoc.getString("title");
+
+                            if (fileName != null) { // filename is unique, skip nulls
+                                ps.setString(1, imageMap.get(key));
+                                ps.setString(2, fileName);
+                                ps.setString(3, title);
+                                ps.addBatch();
+                            }
+                        }
+                    }
+                }
+                ps.executeBatch();
+                System.out.println("Images exported successfully.");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
