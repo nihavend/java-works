fcc47afed572c4f6d0ebca6daa54971140f3191a
Serkan Taş <serkan.tas@trt.net.tr>
2025-10-20 13:32:52 +0300

    .

--- TabiiPersonalization/src/com/tabii/simulation/api/TabiiRestApp.java
+++ TabiiPersonalization/src/com/tabii/simulation/api/TabiiRestApp.java
@@ -3,7 +3,7 @@ package com.tabii.simulation.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.tabii.redisclient", "com.tabii.memcachedclient"})
 public class TabiiRestApp {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(TabiiRestApp.class);
