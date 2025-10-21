85390211c31ae3bc03c9fbcb4f0cce10cfe8f9f6
Serkan Taş <serkan.tas@trt.net.tr>
2025-10-21 14:43:55 +0300

    .

--- TabiiPersonalization/src/com/tabii/utils/CommonUtils.java
+++ TabiiPersonalization/src/com/tabii/utils/CommonUtils.java
@@ -3,9 +3,15 @@ package com.tabii.utils;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 public class CommonUtils {
+	
+	public static final List<String> hcMaps = Arrays.asList("imagesMap", "exclusiveBadgesMap", "badgesMap", "genreMap", "showsMap");
 	
 	private static Properties loadDbProperties() {
 		
@@ -66,5 +72,49 @@ public class CommonUtils {
 		int expireSeconds = Integer.parseInt(dbProps.getProperty("memcached.expire.seconds"));
 		
 	    return new MemcachedProperties(url, expireSeconds);
+	}
+	
+	public static HazelcastProperties getHazelcastConnectionProps() {
+		
+	    Properties dbProps = CommonUtils.loadDbProperties();
+	    
+		String url = dbProps.getProperty("hazelcast.servers");
+		String clusterName = dbProps.getProperty("cluster.name");
+		
+	    return new HazelcastProperties(url, clusterName);
+	}
+	
+	public static List<InetSocketAddress> getServers(String serversStr) {
+		List<InetSocketAddress> serverList = new ArrayList<>();
+		for (String server : serversStr.split(",")) {
+			String[] parts = server.split(":");
+			serverList.add(new InetSocketAddress(parts[0], Integer.parseInt(parts[1])));
+		}
+
+		return serverList;
+	}
+	
+	public static List<String> serversSplitter(String servers) {
+		List<String> addresses = List.of(servers.split(","));
+		
+		return addresses;
+	}
+	
+	public static String mapLocation(String key) {
+		switch (key) {
+		case "upper_right_corner":
+			return "rightTop";
+		case "upper_left_corner":
+			return "leftTop";
+		case "lower_left_corner":
+			return "leftBottom";
+		case "on_top_of_the_logo":
+			return "upLogo";
+		case "under_the_logo":
+			return "bottomLogo";
+		case "do_not_show":
+		default:
+			return "invisible";
+		}
 	}
 }
